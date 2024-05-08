aa4dde8f3c9c5193f938d4d1eaddae7027881e13
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-07 21:50:42 -0400

    updated with an if statment

--- timer1.js
+++ timer1.js
@@ -1,21 +1,16 @@
 // imput from the terminal (example): node timer1.js 1, 3, 6, 8, 11
 const alarmTimes = process.argv.slice(2);
-// process.argv take the input from the terminal and returns array 
+// process.argv take the input from the terminal and returns array
 // with ***"STRING"!!!*** values
 
 for (let num of alarmTimes) {
-    time = parseInt(num) * 1000
-    // convert string num to integer and change to ms
+  time = parseInt(num) * 1000;
+  // convert string num to integer and change to ms
+  if (typeof time === "number" && time > 0) {
     setTimeout(() => {
-    console.log(`beep it's been: ${num} second(s)`), 
-    process.stdout.write("\x07");
-    }, (time));
+      console.log(`beep it's been: ${num} second(s)`),
+        process.stdout.write("\x07");
+      // ^ provides the beep sound
+    }, time);
+  }
 }
-
-
-
-
-
-
-
-
